448247ff6fc9656fabe83c0656cbd0290b18386a
quangtv13 <kwangtran229@gmail.com>
2019-11-07 07:18:32 +0700

    commit

---
+++ Tweetbook/Controllers/V1/Test2Controller.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tweetbook.Controllers.V1
+{
+    public class Test2Controller
+    {
+        //Ignore controller nay
+        //lan 1
+    }
+}
